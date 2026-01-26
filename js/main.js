450369ef2c28331bb7126626765b0069a9c440c0
webdev-jpn <129825530+webdev-jpn@users.noreply.github.com>
2026-01-26 13:16:01 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,76 @@
+// Input elements
+const billInput = document.getElementById("bill");
+const peopleInput = document.getElementById("people");
+const tipButtons = document.querySelectorAll(".user-select");
+const customInput = document.getElementById("custom");
+
+// Output elements
+const tipAmountOutput = document.getElementById("output-tip");
+const totalOutput = document.getElementById("output-total");
+
+// Reset button
+const resetBtn = document.getElementById("reset");
+
+let billValue = 0;
+let peopleValue = 0;
+let btnTipPercent = 0;
+let customTipPercent = 0;
+
+billInput.addEventListener("input", (e) => {
+  billValue = parseFloat(billInput.value) || 0;
+  calculate();
+});
+
+peopleInput.addEventListener("input", (e) => {
+  peopleValue = parseFloat(peopleInput.value) || 0;
+  calculate();
+});
+
+// Tip select buttons
+tipButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    btnTipPercent = parseFloat(button.value);
+    customTipPercent = 0;
+    customInput.value = "";
+    calculate();
+  });
+});
+
+// Custom tip input
+customInput.addEventListener("input", (e) => {
+  customTipPercent = parseFloat(customInput.value) || 0;
+  calculate();
+});
+
+function calculate() {
+  let finalTip = 0;
+  if (customTipPercent > 0) {
+    finalTip = customTipPercent;
+  } else {
+    finalTip = btnTipPercent;
+  }
+
+  if (peopleValue >= 1) {
+    const totalTip = billValue * (finalTip / 100);
+    const tipPerPerson = totalTip / peopleValue;
+    const totalPerPerson = billValue / peopleValue + tipPerPerson;
+
+    tipAmountOutput.textContent = `$${tipPerPerson.toFixed(2)}`;
+    totalOutput.textContent = `$${totalPerPerson.toFixed(2)}`;
+  }
+}
+
+// Rest button
+resetBtn.addEventListener("click", (e) => {
+  billValue = 0;
+  peopleValue = 0;
+  btnTipPercent = 0;
+  customTipPercent = 0;
+
+  billInput.value = "";
+  peopleInput.value = "";
+  customInput.value = "";
+
+  tipAmountOutput.textContent = "$0.00";
+  totalOutput.textContent = "$0.00";
+});
